751975bfb947109934b942e9f7e62da746ccd14e
YuriODantas <iruy3343@gmail.com>
2022-10-21 18:10:09 -0300

    initial commit

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const connection = new Sequelize(process.env.DB_NAME, process.env.USER_DB, process.env.PASSWORD_DB, {
+  host: "localhost",
+  dialect: "mysql"
+});
+
+module.exports = connection;
