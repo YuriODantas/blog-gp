1f598016e53b1c72ebf25ebd3e7c7a53fcf64966
YuriODantas <iruy3343@gmail.com>
2022-10-29 13:36:00 -0300

    feat(database): configure timezone for Brazil

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize(process.env.DB_NAME, process.env.USER_DB, process.env.PASSWORD_DB, {
   host: "localhost",
-  dialect: "mysql"
+  dialect: "mysql",
+  timezone: "-03:00"
 });
 
 module.exports = connection;
