0a4842035b195613791da37df2c9a43e8762e585
YuriODantas <iruy3343@gmail.com>
2022-11-01 20:14:34 -0300

    feat(user): create structure

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const connection = require("./database/database")
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/UsersController")
 
 const Article = require("./articles/Article")
 const Category = require("./categories/Category")
+const User = require("./users/User")
 
 app.set('view engine', 'ejs');
 
@@ -25,6 +27,7 @@ connection.authenticate().then(() => {
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", usersController);
 
 app.get('/', (req, res) => {
   Article.findAll({
